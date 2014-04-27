cbb82817f68178363cfef01a3ce3bec7c2842537
Ron Kurr <rkurr@transparent.com>
2014-04-26 22:35:06 -0400

    Failed attempt at getting SRP working According to the net, the BC library isn't complete and leaves details to the user.  Not what I'm looking for.

---
+++ src/test/groovy/org/kurron/srp/BouncyCastleUnitTest.groovy
@@ -0,0 +1,70 @@
+package org.kurron.srp
+
+import org.bouncycastle.crypto.Digest
+import org.bouncycastle.crypto.agreement.srp.SRP6Client
+import org.bouncycastle.crypto.agreement.srp.SRP6Server
+import org.bouncycastle.crypto.digests.SHA256Digest
+import spock.lang.Specification
+
+import java.security.SecureRandom
+
+/**
+ * Created by vagrant on 4/26/14.
+ */
+class BouncyCastleUnitTest extends Specification {
+    static final SecureRandom random = new SecureRandom()
+
+    def 'exercise simple math'() {
+        // pre-shared by user and host
+        def safePrime = randomNumber()
+        def generator = randomNumber()
+        def hashFunction = digest()
+
+        given: 'a valid user'
+        def user = new SRP6Client()
+
+        user.init( safePrime, generator, hashFunction, random )
+
+        and: 'a valid host'
+        def host = new SRP6Server()
+        def verifier = randomNumber() // supposed to be cryptographically computed
+        host.init( safePrime, generator, verifier, hashFunction, random )
+
+        when: 'an authentication attempt is made'
+        def hostCredentials = host.generateServerCredentials()
+        def userCredentials = user.generateClientCredentials( salt(), identity(), password() )
+        def sharedSecret = host.calculateSecret( userCredentials )
+        def verificationMessage = user.calculateSecret( hostCredentials )
+
+        then: 'authentication is confirmed'
+        sharedSecret == verificationMessage
+    }
+
+    BigInteger randomNumber() {
+        def buffer = new byte[128]
+        random.nextBytes( buffer )
+        new BigInteger( buffer )
+    }
+
+    Digest digest() {
+        new SHA256Digest()
+    }
+
+    byte[] salt() {
+        def salt = new byte[128]
+        random.nextBytes( salt )
+        salt
+    }
+
+    byte[] identity() {
+        def salt = new byte[128]
+        random.nextBytes( salt )
+        salt
+    }
+
+    byte[] password() {
+        def password = new byte[128]
+        random.nextBytes( password )
+        password
+    }
+}
